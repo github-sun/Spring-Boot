b1ce91e05689b8e001002df91034e3130fd223ce
Unknown <g.sunzc@gmail.com>
2018-01-08 16:29:21 +0800

    JDBC Tests

--- src/test/java/AppSuite.java
+++ src/test/java/AppSuite.java
@@ -5,10 +5,11 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.sun.bootapp.AppTests;
 import org.sun.bootapp.dao.UserDAOTest;
+import org.sun.bootapp.dao.UserJdbcDAOTest;
 import org.sun.bootapp.service.UserServiceTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({UserDAOTest.class, UserServiceTest.class, AppTests.class}) 
+@SuiteClasses({UserDAOTest.class, UserJdbcDAOTest.class, UserServiceTest.class, AppTests.class}) 
 public class AppSuite {
 
 }
