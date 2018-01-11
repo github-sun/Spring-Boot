2945fc315ea04060289db9f3510ea44ecbfa38b1
Unknown <g.sunzc@gmail.com>
2018-01-11 14:14:07 +0800

    actuator

--- src/main/java/org/sun/bootapp/config/ws/WebServicesConfig.java
+++ src/main/java/org/sun/bootapp/config/ws/WebServicesConfig.java
@@ -1,8 +1,6 @@
 package org.sun.bootapp.config.ws;
 
-import java.nio.charset.Charset;
 import java.util.Arrays;
-import java.util.List;
 
 import org.apache.cxf.Bus;
 import org.apache.cxf.endpoint.Server;
@@ -10,8 +8,6 @@ import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.sun.bootapp.ws.UserWebServices;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
